56bf4757dd58e8317b7b899aa4f9d24d8c416164
Prashant Nidgunde <pashnidgunde@gmail.com>
2023-09-22 20:24:59 -0400

    FTT : Staement add, replace and remove tags

--- platform/FixTagTransformer/include/FixTagTransformer.h
+++ platform/FixTagTransformer/include/FixTagTransformer.h
@@ -8,71 +8,133 @@
 #include <boost/spirit/home/x3.hpp>
 #include <boost/spirit/home/x3/support/ast/variant.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/spirit/home/x3/core/parse.hpp>
 
 #include <string>
 #include <list>
 #include <numeric>
 #include <iostream>
 
-//namespace client {
-//    namespace ast {
-//        struct program {
-//
-//        };
-//
-//        struct printer {
-//
-//        };
-//
-//        struct eval {
-//
-//        };
-//    }
-//
-//
-//}
-//
-//namespace client {
-//    namespace ftt_grammer {
-//
-//    }
-//}
+namespace x3 = boost::spirit::x3;
 
+//namespace ast {
+//    struct printer {
+//        void operator() (std::pair <uint32_t, const std::string> result) {
+//            std::cout << result.first << result.second;
+//        }
+//    };
+//}
+//
+//namespace grammer {
+//    
+//    x3::rule<class statement, std::pair <uint32_t, const std::string>> const statement("statement");
+//    BOOST_SPIRIT_DEFINE(
+//        statement
+//    );
+//
+//    auto ftt = statement;
+//}
+//
+//
+//struct eval {
+//    
+//    
+//
+//};
+
+
+struct Statement {
+    Statement(const std::string message) : _message(message) {
+    }
+
+    std::string _message;
+
+    template<typename Iter>
+    bool apply(Iter begin, Iter end) {
+        using x3::uint_;
+        using x3::char_;
+        using x3::alnum;
+        using boost::spirit::x3::ascii::space_type;
+        using x3::_attr;
+        space_type space;
+
+        const auto messgage_begin = _message.begin();
+
+        auto tag_start = std::string::npos;
+        auto value_start = std::string::npos;
+        auto value_end = std::string::npos;
+        auto tag_end = std::string::npos;
+         
+
+        auto tag = [&](auto& tag) { 
+            const auto& tag_number = _attr(tag);
+            tag_start = _message.find(tag_number);
+            if (tag_start == std::string::npos) {
+                tag_start = _message.size();
+                _message += tag_number + '=';
+                return;
+            }
+            value_start = tag_start + tag_number.size() + 1;
+            value_end = _message.find_first_of(';', value_start);
+            tag_end = value_end + 1;
+        };
+        
+        auto value = [&](auto& value) { 
+            const auto& tag_value = _attr(value);
+            //std::cout << "Value :" << _attr(value) << typeid(_attr(value)).name() << "\n"; 
+            if (_message.back() == '=') {
+                if (!tag_value.empty()) {
+                    _message += tag_value + ";";
+                }
+                else {
+                    _message.erase(messgage_begin + tag_start, _message.end());
+                }
+                return;
+            }
+            
+            if (tag_value.empty()) {
+                _message.erase(messgage_begin + tag_start, messgage_begin + tag_end);
+            }
+            else {
+                _message.replace(messgage_begin + value_start, messgage_begin + value_end, tag_value);
+            }
+        };
+       
+        auto statement_rule = (+(char_ - '='))[tag] >> '=' >> (*(char_ - ';'))[value] >> ';';
+        //auto statement = ((+(char_ - '=')) >> char_('='))[tag] >> (+(char_ - ';'))[value] >> ';';
+  
+
+        bool r = x3::phrase_parse(begin, end, statement_rule, space);
+
+        if (begin != end) // fail if we did not get a full match
+            return false;
+        return r;
+    }
+
+    bool apply(const std::string& statement) {
+        return apply(statement.begin(), statement.end());
+    }
+
+    const std::string& appplied() const { return _message; }
+};
 
 struct FixTagTransformer {
 	FixTagTransformer() = delete;
-	explicit FixTagTransformer(const std::vector<std::string>& rules) :
+	explicit FixTagTransformer(const std::string& _message, const std::vector<std::string>& rules) :
+        _statement(_message),
 		_rules(rules)
 	{}
 
+    Statement _statement;
 	std::vector<std::string> _rules;
 
-	static bool transform(const std::string& str) {
-        
-        /*using iterator_type = std::string::const_iterator ;
-        using ast_program = client::ast::program;
-        using ast_print = client::ast::printer;
-        using ast_eval = client::ast::eval;*/
-
-        //auto& ftt = client::ftt;           // Our grammar
-        //ast_program program;                // Our program (AST)
-        //ast_print print;                    // Prints the program
-        //ast_eval eval;                      // Evaluates the program
-        
-        namespace x3 = boost::spirit::x3;
-        using x3::uint_;
-        using x3::alnum;
-        using boost::spirit::x3::ascii::space_type;
-
-        auto statement = (+(uint_)) >> '=' >> *alnum >> ';';
-        //auto statement = (+(char_)-'=') >> '=';
-        //auto statement = (+(char_)-'=') >> '=' >> (+(char_)-';') >> ';';
-        //auto tag_values = (statement);
-
-        auto begin = str.begin();
-        auto end = str.end();
-        space_type space;
-        bool r = phrase_parse(begin, end, statement, space);
-        return r && begin == end;
+	bool apply() {
+        for (const auto& rule : _rules) {
+            _statement.apply(rule.begin(), rule.end());
+            std::cout << _statement.appplied();
+        }
+        return true;
 	}
+
+    const std::string& applied() { return _statement.appplied(); }
 };
